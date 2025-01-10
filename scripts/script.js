312a6822619154b167e73f185db7e99644c0dcc8
Irie Judicael Elvis <iriejudicaelelvis225@gmail.com>
2025-01-10 15:24:55 +0000

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+// Get references to the menu icon and the navigation menu
+const menuBar = document.getElementById('menu-bar');
+const navMenu = document.getElementById('nav-menu');
+
+// Add a click event listener to the menu icon
+menuBar.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
 // Get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
